af69a34a40493465635cfa3c2b3ec8c4214ed988
Justin Young <jyoung7223@yahoo.com>
2020-12-07 18:36:09 -0700

    Add Role functionality

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Required packages & Libraries
 const Inquirer = require("inquirer");
 const DB = require("./utils/Database.js");
+const MySQL = require("mysql2");
 const Employee = require("./lib/Employee.js");
 const Role = require("./lib/Role.js");
 const Department = require("./lib/Department.js");
@@ -45,12 +46,18 @@ function start() {
                 start();
                 break;
             case "View All Roles":
-                role.selectRoles();
+                role.printRoles();
                 start();
                 break;
             case "View All Departments":
-                department.selectDepartments();
+                department.printDepartments();
                 start();
+                break;
+            case "Add Role":
+                addRole();
+                break;
+            case "Add Role":
+                addRole();
                 break;
             case "Add Department":
                 addDepartment();
@@ -66,20 +73,58 @@ function start() {
     });
 }
 
-// This function will handle adding a department
-function addDepartment(){
-    let question = "What department would you like to add?";
-    Inquirer.prompt(
-        {
-            name: "department",
-            type: "input",
-            message: question            
+// This function will handle adding a role
+function addRole() {
+    let departments = [];
+    // First get the list of departments    
+    DB.query("SELECT * FROM department",
+        function (err, res) {
+            if (err) console.log(err);
+            for (let i = 0; i < res.length; i++){
+                if(res[i].name){
+                    departments.push(res[i].name);
+                }
+            }            
+            
+            // Get the role details
+            let questions = [
+                "What is the role title you would like to add?",
+                "What is the role salary?",
+                "What is the role department?"];
+            Inquirer.prompt([
+                {
+                    name: "title",
+                    type: "input",
+                    message: questions[0]
+                },
+                {
+                    name: "salary",
+                    type:"number",
+                    message:questions[1]
+                },
+                {
+                    name:"department",
+                    type:"list",
+                    message: questions[2],
+                    choices: departments
+                }
+            ]).then((data) => {
+                // get the department to tie to 
+                let departmentId = 0;
+                for(let i=0; i<res.length; i++){
+                    if(res[i].name === data.department){
+                        departmentId = res[i].id;
+                        break;                        
+                    }
+                }
+                role.insertRole(data.title, data.salary, departmentId);
+                start();
+            });
+
         }
-    ).then((data) => {
-        department.insertDepartment(data.department);
-        start();
-    });
+    );
 }
+
 // This function will handle adding a role
 function addDepartment(){
     let question = "What department would you like to add?";
