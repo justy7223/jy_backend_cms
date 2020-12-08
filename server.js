966dcc77ab036ae1702c915896715654cde9cd5f
Justin Young <jyoung7223@yahoo.com>
2020-12-07 19:54:28 -0700

    Add Employee & Update Role

--- server.js
+++ server.js
@@ -17,19 +17,19 @@ function start() {
     let question = "What would you like to do?";
     let options = [
         "View All Employees",
-        "View All Employees By Department",
-        "View All Employees By Manager",
+        // "View All Employees By Department",
+        // "View All Employees By Manager",
         "Add Employee",
-        "Remove Employee",
+        // "Remove Employee",
         "Update Employee Role",
-        "Update Employee Manager",
+        // "Update Employee Manager",
         "View All Roles",
         "Add Role",
-        "Remove Role",
-        "Update Role Department",
+        // "Remove Role",
+        // "Update Role Department",
         "View All Departments",
         "Add Department",
-        "Remove Department",
+        // "Remove Department",
         "Exit"
     ];
     Inquirer.prompt(
@@ -53,14 +53,17 @@ function start() {
                 department.printDepartments();
                 start();
                 break;
-            case "Add Role":
-                addRole();
+            case "Add Employee":
+                addEmployee();
                 break;
             case "Add Role":
                 addRole();
                 break;
             case "Add Department":
                 addDepartment();
+                break;
+            case "Update Employee Role":
+                updateEmployeeRole();
                 break;
             case "Exit":
                 console.log("Thank you for using our HR Employee Tracker. Have a great day.");
@@ -74,8 +77,23 @@ function start() {
 }
 
 // This function will handle adding a role
+function addDepartment(){
+    let question = "What department would you like to add?";
+    Inquirer.prompt(
+        {
+            name: "department",
+            type: "input",
+            message: question            
+        }
+    ).then((data) => {
+        department.insertDepartment(data.department);
+        start();
+    });
+}
+
+// This function will handle adding a role
 function addRole() {
-    let departments = [];
+    let departments = ["No Department"];
     // First get the list of departments    
     DB.query("SELECT * FROM department",
         function (err, res) {
@@ -110,7 +128,7 @@ function addRole() {
                 }
             ]).then((data) => {
                 // get the department to tie to 
-                let departmentId = 0;
+                let departmentId = null;
                 for(let i=0; i<res.length; i++){
                     if(res[i].name === data.department){
                         departmentId = res[i].id;
@@ -125,68 +143,156 @@ function addRole() {
     );
 }
 
-// This function will handle adding a role
-function addDepartment(){
-    let question = "What department would you like to add?";
-    Inquirer.prompt(
-        {
-            name: "department",
-            type: "input",
-            message: question            
+// This function will handle adding an employee
+function addEmployee() {
+    let roles = ["No Role"];
+    let managers = ["No Manager"];
+    // First get the list of roles    
+    DB.query("SELECT * FROM role ",
+        function (err, roleRes) {
+            if (err) console.log(err);
+            for (let i = 0; i < roleRes.length; i++){
+                if(roleRes[i].title){
+                    roles.push(roleRes[i].title);
+                }
+            }
+            
+            // Next get list of possible managers
+            DB.query("SELECT * from employee ", 
+                function (err, empRes) {
+                    if(err) console.log(err);
+                    for (let i = 0; i < empRes.length; i++){
+                        if(empRes[i].first_name){
+                            managers.push(empRes[i].first_name+" "+empRes[i].last_name);
+                        }
+                    }
+            
+                    // Get the employee details
+                    let questions = [
+                        "What is the employee first name?",
+                        "What is the employee last name?",
+                        "What is the employee role?",
+                        "Who is the employee manager?"];
+                    Inquirer.prompt([
+                        {
+                            name: "firstName",
+                            type: "input",
+                            message: questions[0]
+                        },
+                        {
+                            name: "lastName",
+                            type:"input",
+                            message:questions[1]
+                        },
+                        {
+                            name:"role",
+                            type:"list",
+                            message: questions[2],
+                            choices: roles
+                        },
+                        {
+                            name: "manager",
+                            type:"list",
+                            message:questions[3],
+                            choices: managers
+                        }
+                    ]).then((data) => {
+                        // get the role to tie to 
+                        let roleId = null;
+                        for(let i=0; i<roleRes.length; i++){
+                            if(roleRes[i].title === data.role){
+                                roleId = roleRes[i].id;
+                                break;                        
+                            }
+                        }
+                        // Get the manager to tie to
+                        let managerId = null;
+                        for (let i=0; i<empRes.length; i++){
+                            if(empRes[i].first_name+" "+empRes[i].last_name === data.manager ){
+                                managerId = empRes[i].id;
+                                break;
+                            }
+                        }
+                        employee.insertEmployee(data.firstName, data.lastName, roleId, managerId);
+                        start();
+                    });
+
+                }
+            );
         }
-    ).then((data) => {
-        department.insertDepartment(data.department);
-        start();
-    });
+    );
 }
 
-// This function will handle the add employee requests
-function addEmployee(){
-    let questions = [
-        "What is employee's first name?",
-        "What is employee's last name?",
-        "What is employee's role?",
-        "Who is employee's manager?"];
-    
-    let roles = role.selectRoles();
-    let employees = employee.selectEmployees();
-    let roleOptions = [];
-    let managerOptions = [];
-    for (let i=0; i< roles.length; i++){
-        roleOptions.push(roles[i].title);
-    }
-    roleOptions.push("No Role (Free-Loader)");
-    for (let i=0; i< employees.length; i++){
-        managerOptions.push(employees[i].first_name+" "+employees[i].last_name);
-    }
-    managerOptions.push("No Manager");
-   
-    Inquirer.prompt(
-        {
-            name: "firstName",
-            type: "input",
-            message: questions[0]
-        },
-        {
-            name: "lastName",
-            type: "input",
-            message: questions[1]
-        },
-        {
-            name:"role",
-            type:"list",
-            message:question[2],
-            choices:roleOptions
-        },
-        {
-            name:"manager",
-            type:"list",
-            message:question[3],
-            choices: managerOptions
+
+
+
+// This function will handle updating an employee role
+function updateEmployeeRole() {
+    let roles = ["No Role"];
+    let employees = [];
+    // First get the list of roles    
+    DB.query("SELECT * FROM role ",
+        function (err, roleRes) {
+            if (err) console.log(err);
+            for (let i = 0; i < roleRes.length; i++){
+                if(roleRes[i].title){
+                    roles.push(roleRes[i].title);
+                }
+            }
+            
+            // Next get list of possible managers
+            DB.query("SELECT * from employee ", 
+                function (err, empRes) {
+                    if(err) console.log(err);
+                    for (let i = 0; i < empRes.length; i++){
+                        if(empRes[i].first_name){
+                            employees.push(empRes[i].first_name+" "+empRes[i].last_name);
+                        }
+                    }
+            
+                    // Get the employee details
+                    let questions = [
+                        "Who's role would you like to update?",
+                        "What is their new role??"];
+                    Inquirer.prompt([
+                        {
+                            name: "employee",
+                            type: "list",
+                            message: questions[0],
+                            choices: employees
+                        },
+                        {
+                            name: "role",
+                            type:"list",
+                            message:questions[1],
+                            choices: roles
+                        }
+                    ]).then((data) => {
+                        // get the role to tie to 
+                        let roleId = null;
+                        for(let i=0; i<roleRes.length; i++){
+                            if(roleRes[i].title === data.role){
+                                roleId = roleRes[i].id;
+                                break;                        
+                            }
+                        }
+                        // Get the employee to update to
+                        for (let i=0; i<empRes.length; i++){
+                            if(empRes[i].first_name+" "+empRes[i].last_name === data.employee ){
+                                employee.setProperties(empRes[i]);
+                                employee.role_id = roleId;
+                                employee.updateEmployee();
+                                break;
+                                
+                            }
+                        }
+                        start();
+                    });
+
+                }
+            );
         }
-    ).then((data) => {
-        console.log(data);
-    });
+    );
 }
 
 start();
